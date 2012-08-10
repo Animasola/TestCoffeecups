ae0a1b1e66fc2c9a8d6bb9855a5be8ce093337d4
Andrew <annima.sola@gmail.com>
2012-08-10 20:45:45 +0300

    ticket_1 including south, creating models

---
+++ testingproject/testapp/migrations/0001_initial.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'MyInfo'
+        db.create_table('testapp_myinfo', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=50)),
+            ('surname', self.gf('django.db.models.fields.CharField')(max_length=75)),
+            ('birth_date', self.gf('django.db.models.fields.DateField')()),
+            ('bio', self.gf('django.db.models.fields.TextField')()),
+            ('email', self.gf('django.db.models.fields.EmailField')(max_length=75)),
+            ('jabber', self.gf('django.db.models.fields.CharField')(max_length=50)),
+            ('skype', self.gf('django.db.models.fields.CharField')(max_length=50)),
+            ('other_cont', self.gf('django.db.models.fields.TextField')()),
+        ))
+        db.send_create_signal('testapp', ['MyInfo'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'MyInfo'
+        db.delete_table('testapp_myinfo')
+
+
+    models = {
+        'testapp.myinfo': {
+            'Meta': {'object_name': 'MyInfo'},
+            'bio': ('django.db.models.fields.TextField', [], {}),
+            'birth_date': ('django.db.models.fields.DateField', [], {}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'jabber': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'other_cont': ('django.db.models.fields.TextField', [], {}),
+            'skype': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'surname': ('django.db.models.fields.CharField', [], {'max_length': '75'})
+        }
+    }
+
+    complete_apps = ['testapp']
--- testingproject/testapp/models.py
+++ testingproject/testapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class MyInfo(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Name')
+    surname = models.CharField(max_length=75, verbose_name='Last name')
+    birth_date = models.DateField(auto_now=False, auto_now_add=False, verbose_name='Date of birth')
+    bio = models.TextField(verbose_name='Bio')
+    email = models.EmailField(max_length=75, verbose_name='Email')
+    jabber = models.CharField(max_length=50, verbose_name='Jabber')
+    skype = models.CharField(max_length=50, verbose_name='Skype')
+    other_cont = models.TextField()
+
+    def __unicode__(self):
+        return '%s %s' % (self.name, self.surname)
