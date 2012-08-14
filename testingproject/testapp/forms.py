98905a337ae4e1ab3b0f024cbb0164baa54cc0b5
Andrew <annima.sola@gmail.com>
2012-08-14 19:19:10 +0300

    ticket_5 css & stuff

--- testingproject/testapp/forms.py
+++ testingproject/testapp/forms.py
@@ -8,6 +8,7 @@ class MyInfoForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(MyInfoForm, self).__init__(*args, **kwargs)
         self.fields['my_photo'].widget.attrs["onchange"] = "upload_img(this);"
+
     class Media:
         js = ("/static/js/jquery.js",)
 
