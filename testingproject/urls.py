75fdb3df2b469977bbccb2e76f6a0bb9d8739c17
Andrew <annima.sola@gmail.com>
2012-08-16 20:44:13 +0300

    ticket_8 templatetag

--- testingproject/urls.py
+++ testingproject/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.conf import settings
+from django.contrib import admin
+
 from django.contrib.auth.views import login, logout
 from testapp.views import main_page_info, requests_log, edit_my_info
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', main_page_info, name='home'),
@@ -13,6 +13,7 @@ urlpatterns = patterns('',
     url(r'^editinfo/', edit_my_info, name='editinfo'),
     url(r'^accounts/login/$', login, name='login'),
     url(r'^accounts/logout/$', logout, name='logout'),
+    url(r'^admin/', include(admin.site.urls)),
     # Examples:
     # url(r'^$', 'testingproject.views.home', name='home'),
     # url(r'^testingproject/', include('testingproject.foo.urls')),
