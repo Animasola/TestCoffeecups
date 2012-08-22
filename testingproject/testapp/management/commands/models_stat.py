bcbe03a1c8dbfa63ff3f3d9a26761a390fa04a66
Andrew <annima.sola@gmail.com>
2012-08-23 00:24:01 +0300

    final cut

--- testingproject/testapp/management/commands/models_stat.py
+++ testingproject/testapp/management/commands/models_stat.py
@@ -2,13 +2,14 @@ from django.core.management.base import NoArgsCommand
 from sys import stderr
 
 
-class Command( NoArgsCommand ):
-     requires_model_validation = True
+class Command(NoArgsCommand):
+    requires_model_validation = True
 
-     def handle_noargs(self, **options):
-          from django.db.models import get_app, get_models
-          lines = []
-          for model in get_models():
-               lines.append("[%s]" % model.__name__ + " - %s objects" % model._default_manager.count() or "" + "\n")
-          self.stderr.write("error: " + "%s" % "\nerror: ".join( lines ))
-          return "\n".join( lines )
+    def handle_noargs(self, **options):
+        from django.db.models import get_app, get_models
+        lines = []
+        for model in get_models():
+            lines.append("[%s]" % model.__name__ +\
+                " - %s objects" % model._default_manager.count() or "" + "\n")
+        self.stderr.write("error: " + "%s" % "\nerror: ".join(lines))
+        return "\n".join(lines)
