df298810a1f2f55db01f865fa639ef820c51cfc3
Andrew <annima.sola@gmail.com>
2012-08-26 13:43:05 +0300

    ticket_14 views and javaScript

--- testingproject/static_production/js/requests.js
+++
@@ -1,9 +0,0 @@
-function removeRequestInstance(obj, id) {
-	row = $(obj).parents(".log_instance");
-	
-	if (confirm('Are you sure you want to delete this instance?')) {
-		$.getJSON("/remove/?id=" + id, { }, function() {
-			$(obj).parents(".log_instance").animate({opacity: 'hide'}, "slow");
-			});
-	}
-}
